59ab7e0de5328ad82afd728a399ebf38fd27aa6d
Markianos <alexander.markianos@gmail.com>
2021-03-10 20:58:38 +0100

    kommentarer på samtliga

--- upppgift_2/script2.js
+++ upppgift_2/script2.js
@@ -1,32 +1,32 @@
 // alert('min JS koppling fungerar och är redo att rocka');
 
-const todoForm = document.querySelector('#todoForm');               // deklarerar att todoForm refererar till mitt formulär
-const input = document.querySelector('#todoInput')                  // deklarerar att input refererar till id #todoInput 
-const btn = document.querySelector('#btn');                         // deklarerar att btn refererar till min min knapp med id #btn
-const output = document.querySelector('#output');                   // deklarerar att const output är id #output i mitt html dokument
+const todoForm = document.querySelector('#todoForm');                                                   // deklarerar att todoForm refererar till mitt formulär
+const input = document.querySelector('#todoInput')                                                      // deklarerar att input refererar till id #todoInput 
+const btn = document.querySelector('#btn');                                                             // deklarerar att btn refererar till min min knapp med id #btn
+const output = document.querySelector('#output');                                                       // deklarerar att const output är id #output i mitt html dokument
 
-let todos = []                                                     // Skapar en todos array som är en let, vilket senare kan ändras   
+let todos = []                                                                                          // Skapar en todos array som är en let, vilket senare kan ändras   
 
-const fetchTodos = () => {                                          // skapar funktionen fetchTodos
-    //fetch('https://jsonplaceholder.typicode.com/todos/?userId=2/?_limit=10') // hämtar/fetchar vår json placeholders med user id 2 och en limit på 10st objekt
-    fetch('https://jsonplaceholder.typicode.com/todos/?_limit=10')             // hämtar/fetchar vår json placeholders en limit på 10st objekt
-        .then(res => res.json())                                    // resultaten från json databas
-        .then(data => {                                             // datan från json
-            todos = data;                                           // data från json läggs in i vår todos array
+const fetchTodos = () => {                                                                              // skapar funktionen fetchTodos
+    //fetch('https://jsonplaceholder.typicode.com/todos/?userId=2/?_limit=10')                          // hämtar/fetchar vår json placeholders med user id 2 och en limit på 10st objekt
+    fetch('https://jsonplaceholder.typicode.com/todos/?_limit=10')                                      // hämtar/fetchar vår json placeholders en limit på 10st objekt
+        .then(res => res.json())                                                                        // resultaten från json databas
+        .then(data => {                                                                                 // data från json
+            todos = data;                                                                               // data från json läggs in i vår todos array
             console.log(todos);
-            listTodos();                                            // funktionen listTodos()
+            listTodos();                                                                                // funktionen listTodos()
         })
 }
 fetchTodos();
 
-const listTodos = () => {                                           // skapar funktionen listTodos()
-    output.innerHTML = '';                                          // skapar tomt innehåll i min output
-    todos.forEach(todo => {                                         // för varje objekt/todo i min array todos
-        newTodo(todo);                                              // skapa en newTodo() där vi placerar varje objekt
+const listTodos = () => {                                                                               // skapar funktionen listTodos()
+    output.innerHTML = '';                                                                              // skapar tomt innehåll i min output
+    todos.forEach(todo => {                                                                             // för varje objekt/todo i min array todos
+        newTodo(todo);                                                                                  // skapa en newTodo() där vi placerar varje objekt
     })
 }
 
-const newTodo = (todo) => {                                             // skapar funktionen newTodo som tar in "todo"
+const newTodo = (todo) => {                                                                             // skapar funktionen newTodo som tar in "todo"
     
     let taskWrapper = document.createElement('div');                                                    // skapar en div i html med id "taskWrapper"
     taskWrapper.classList.add('border', 'rounded', 'bg-white', 'p-2', 'mb-2');                          // lägger till klasser till vår div
@@ -85,18 +85,18 @@ const addTodo = (title) => {
 
 /* ------------------------------->   Validering av Todo input <------------- */
 
-const todoValidate = (id) => {                                      // Skapar en funktion som heter todovalidate
-    const input = document.querySelector(id);                       // deklarerar en const input som pekar på id
-    const error = document.querySelector(id + '-error');            // deklarerar en const error som pekar på  ID "# + -error"
+const todoValidate = (id) => {                                                                          // Skapar en funktion som heter todovalidate
+    const input = document.querySelector(id);                                                           // deklarerar en const input som pekar på id
+    const error = document.querySelector(id + '-error');                                                // deklarerar en const error som pekar på  ID "# + -error"
 
-    if (input.value.trim() === '') {                                // om värdet är tomt så...
-        error.innerText = "You can't do nothing...Enter a todo please"                     // skriv ut i ID-error  "Enter a todo please"
-        input.focus();                                              // fokus kring input
-        return false;                                               // returnera false dvs. avbryt
+    if (input.value.trim() === '') {                                                                    // om värdet är tomt så...
+        error.innerText = "You can't do nothing...Enter a todo please"                                  // skriv ut i ID-error  "Enter a todo please"
+        input.focus();                                                                                  // fokus kring input
+        return false;                                                                                   // returnera false dvs. avbryt
     }
-    else {                                                          // annars...
-        error.innerText = ''                                        // skriv inte ut något i ID-error    
-        return true;                                                // returnera true och gå vidare
+    else {                                                                                              // annars...
+        error.innerText = ''                                                                            // skriv inte ut något i ID-error    
+        return true;                                                                                    // returnera true och gå vidare
     }
 }
 
@@ -104,16 +104,12 @@ const todoValidate = (id) => {                                      // Skapar en
 todoForm.addEventListener('submit', e => {
     e.preventDefault();
 
-    todoValidate('#todoInput');                                              // kör vår todoValidate funktion på id #todoInput
+    todoValidate('#todoInput');                                                                         // kör vår todoValidate funktion på id #todoInput
       
-      if( todoValidate('#todoInput')){                                       // Om funktionen todoValidate går igenom på #todoInput så,,,
-        addTodo(input.value)
-        input.value = '';                                           // kör createUser funktionen med värdet i todoInput
+      if( todoValidate('#todoInput')){                                                                  // Om funktionen todoValidate går igenom på #todoInput så...
+        addTodo(input.value)                                                                            // kör funktionen addTodo med värdet som finns i input.value
+        input.value = '';                                                                               // ändra värdet i input till tomt
       }
 
 })
 
-output.addEventListener('click', e => {
-    todos = todos.filter(todo => todo.id !== e.target.parentNode.id)
-    listTodos();
-})
